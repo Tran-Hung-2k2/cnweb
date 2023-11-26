618d9b2c9d0a1054308a85aee3724f0fdb608388
dinhhuong2002 <dinhthihuongdung2002@gmail.com>
2023-11-26 18:25:01 +0700

    commit

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+import Home from "../components/Home";
+
+export default function HomePage() {
+    return (
+        <>
+            <Header>
+            </Header>
+            <Home/>
+        </>
+    );
+}
