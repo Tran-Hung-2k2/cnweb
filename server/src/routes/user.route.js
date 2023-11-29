816edcc5228e4d5f6d1bc853a998f823cb580f62
TruongHung02 <truongviethung2682002@gmail.com>
2023-11-29 22:11:57 +0700

    update user route find user by id

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -5,7 +5,8 @@ import ctrl from '../controllers/user.controller.js';
 
 const route = express.Router();
 
-route.get('/', ctrl.getAllUser);
+route.get('/', ctrl.get_all_user);
+route.get('/:id', ctrl.get_user_by_id)
 
 
 export default route;
