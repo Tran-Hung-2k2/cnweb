486103c94f7a501899eefcc2496638d855cd96c8
Tran Hung <tranviethung912002@gmail.com>
2023-12-02 03:35:12 +0700

    add label and fields

---
+++ client1/src/constants/signinFields.jsx
@@ -0,0 +1,18 @@
+const fields = [
+    {
+        label: 'Email',
+        id: 'Email',
+        type: 'email',
+        required: false,
+        placeholder: 'Địa chỉ Email',
+    },
+    {
+        label: 'Mật khẩu',
+        id: 'Password',
+        type: 'password',
+        required: true,
+        placeholder: 'Mật khẩu',
+    },
+];
+
+export default fields;
