460c95d4b8c716b57b890bdcb54cc8cf0a7a440c
Tran Hung <tranviethung912002@gmail.com>
2023-12-01 20:56:34 +0700

    update layout

--- client1/src/layout/DefaultLayout.jsx
+++ client1/src/layout/DefaultLayout.jsx
@@ -4,34 +4,17 @@ import Sidebar from '../components/Sidebar';
 import { Outlet } from 'react-router-dom';
 
 const DefaultLayout = () => {
-    const [sidebarOpen, setSidebarOpen] = useState(false);
-
     return (
-        <div className="dark:bg-boxdark-2 dark:text-bodydark">
-            {/* <!-- ===== Page Wrapper Start ===== --> */}
-            <div className="flex h-screen overflow-hidden">
-                {/* <!-- ===== Sidebar Start ===== --> */}
-                <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-                {/* <!-- ===== Sidebar End ===== --> */}
-
-                {/* <!-- ===== Content Area Start ===== --> */}
-                <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-                    {/* <!-- ===== Header Start ===== --> */}
-                    <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-                    {/* <!-- ===== Header End ===== --> */}
-
-                    {/* <!-- ===== Main Content Start ===== --> */}
-                    <main>
-                        <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-                            <Outlet />
-                        </div>
-                    </main>
-                    {/* <!-- ===== Main Content End ===== --> */}
-                </div>
-                {/* <!-- ===== Content Area End ===== --> */}
+        <main className="flex">
+            <div className="relative h-full drawer drawer-open max-w-fit">
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+                <Sidebar />
             </div>
-            {/* <!-- ===== Page Wrapper End ===== --> */}
-        </div>
+            <div className="w-screen">
+                <Header />
+                <Outlet />
+            </div>
+        </main>
     );
 };
 
