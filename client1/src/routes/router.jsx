1c8aea2e59b30a99bced3bdfba9094c99c699183
Tran Hung <tranviethung912002@gmail.com>
2023-12-02 03:38:44 +0700

    update router

--- client1/src/routes/router.jsx
+++ client1/src/routes/router.jsx
@@ -1,12 +1,18 @@
 import { lazy } from 'react';
 
 const Profile = lazy(() => import('../pages/Profile'));
+const CourseManager = lazy(() => import('../pages/CourseManager'));
 
 const coreRoutes = [
     {
         path: '/profile',
-        title: 'Profile',
+        title: 'Thông tin người dùng',
         component: Profile,
+    },
+    {
+        path: '/course_manager',
+        title: 'Quản lý khóa học',
+        component: CourseManager,
     },
 ];
 
