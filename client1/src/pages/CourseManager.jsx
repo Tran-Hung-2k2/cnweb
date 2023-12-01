66fc557489d7be87439f69d3a8fd13098966eef1
Tran Hung <tranviethung912002@gmail.com>
2023-12-02 03:37:52 +0700

    add course manager page signin, notfound

--- client1/src/pages/CourseManager.jsx
+++ client1/src/pages/CourseManager.jsx
@@ -1,14 +1,14 @@
-const ECommerce = () => {
+const CourseManager = () => {
     return (
         <>
-            <div className="card w-96 bg-base-100 shadow-xl w-">
+            <div className="shadow-xl card w-96 bg-base-100 w-">
                 <figure>
                     <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">Shoes!</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
+                    <div className="justify-end card-actions">
                         <button className="btn btn-primary">Buy Now</button>
                     </div>
                 </div>
@@ -17,4 +17,4 @@ const ECommerce = () => {
     );
 };
 
-export default ECommerce;
+export default CourseManager;
---
+++ client1/src/pages/SignUp.jsx
@@ -0,0 +1,6 @@
+
+const SignUp = () => {
+    return <></>;
+};
+
+export default SignUp;
