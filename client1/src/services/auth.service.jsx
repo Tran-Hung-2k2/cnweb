b78dd8cef8682ccef92bba16709479d6fa5cb0f1
Tran Hung <tranviethung912002@gmail.com>
2023-12-03 03:36:16 +0700

    update auth service

--- client1/src/services/auth.service.jsx
+++ client1/src/services/auth.service.jsx
@@ -1,18 +1,17 @@
-import axiosService from './axios.service';
+import axios from './axios.service';
 
 const userService = {
-    login: async (data) => {
-        return await axiosService.post('/api/auth/login', JSON.stringify(data), {
-            headers: { 'Content-Type': 'application/json' },
-            withCredentials: true,
-        });
-    },
-    register: async (data) => {
-        return await axiosService.post('/api/auth/register', data);
-    },
-    logout: async () => {
-        return await axiosService.post('/api/auth/logout');
-    },
+    login: async (data) =>
+        (
+            await axios.post('/api/auth/login', JSON.stringify(data), {
+                headers: { 'Content-Type': 'application/json' },
+                withCredentials: true,
+            })
+        ).data,
+
+    register: async (data) => (await axios.post('/api/auth/register', data)).data,
+
+    logout: async () => (await axios.post('/api/auth/logout')).data,
 };
 
 export default userService;
