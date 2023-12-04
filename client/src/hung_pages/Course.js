fb87795de34ac39d2a328149687604dbf7c93d8a
hungtq0712 <toquanghung0712@gmail.com>
2023-12-04 17:47:57 +0700

    update course

--- client/src/hung_pages/Course.js
+++ client/src/hung_pages/Course.js
@@ -1,15 +1,23 @@
 export default function Course() {
     return (
-        <section>
-            <h2>Chuyên môn cơ bản về AI nhân tạo</h2>
-            <p>Mở khóa và tận dụng tiềm năng của AI nhân tạo. Tìm hiểu cách bạn có thể sử dụng các khả năng của AI tổng hợp để nâng cao công việc và cuộc sống hàng ngày của mình.</p>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                <div>
+                    <h1>Chuyên môn cơ bản về AI nhân tạo</h1>
+                    <p className="py-6">Mở khóa và tận dụng tiềm năng của AI nhân tạo. Tìm hiểu cách bạn có thể sử dụng các khả năng của AI tổng hợp để nâng cao công việc và cuộc sống hàng ngày của mình.</p>
+                    <h3>Chứng Chỉ Đạt Được</h3>
+                    <ul>
+                        <li>Đạt được kỹ năng cơ bản về AI.</li>
+                        <li>Xây dựng ứng dụng sử dụng AI.</li>
+                        <li>Hiểu biết về các khái niệm quan trọng ,...</li>
+                    </ul>
+                    <button className="btn btn-primary">Bắt đầu</button>
+                </div>
+            </div>
+        </div>
 
-            <h3>Chứng Chỉ Đạt Được</h3>
-            <ul>
-                <li>Đạt được kỹ năng cơ bản về AI.</li>
-                <li>Xây dựng ứng dụng sử dụng AI.</li>
-                <li>Hiểu biết về các khái niệm quan trọng ,...</li>
-            </ul>
-        </section>
+
     );
 }
+
