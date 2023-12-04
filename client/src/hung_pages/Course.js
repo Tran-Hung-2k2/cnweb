56be9d262542cda0e491e1b12e5d84a4acfc73f4
hungtq0712 <toquanghung0712@gmail.com>
2023-12-04 17:38:53 +0700

    course introduction

---
+++ client/src/hung_pages/Course.js
@@ -0,0 +1,15 @@
+export default function Course() {
+    return (
+        <section>
+            <h2>Chuyên môn cơ bản về AI nhân tạo</h2>
+            <p>Mở khóa và tận dụng tiềm năng của AI nhân tạo. Tìm hiểu cách bạn có thể sử dụng các khả năng của AI tổng hợp để nâng cao công việc và cuộc sống hàng ngày của mình.</p>
+
+            <h3>Chứng Chỉ Đạt Được</h3>
+            <ul>
+                <li>Đạt được kỹ năng cơ bản về AI.</li>
+                <li>Xây dựng ứng dụng sử dụng AI.</li>
+                <li>Hiểu biết về các khái niệm quan trọng ,...</li>
+            </ul>
+        </section>
+    );
+}
