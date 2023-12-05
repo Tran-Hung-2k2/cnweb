3b70abe736223252c0a87c5075ff2e13a3bda5d3
dinhhuong2002 <dinhthihuongdung2002@gmail.com>
2023-12-06 00:35:08 +0700

    import Career_item to Career with example

--- client/src/huong_pages/DegreesPage.js
+++ client/src/huong_pages/DegreesPage.js
@@ -1,20 +1,34 @@
 import Degrees from "../huong_components/Degrees";
 export default function DegreesPage() {
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">
-                    Shoes!
-                    <div className="badge badge-secondary">NEW</div>
-                </h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div>
-                    <div className="badge badge-outline">Products</div>
+        <>
+            <div class="cds-9 css-fm49zv cds-10 cds-13">
+                <div class="cds-9 css-10f69r4">
+                    <h1 class="cds-113 cds-Typography-base css-1xy8ceb cds-115">
+                        Launch your new career with a Professional Certificate on Coursera</h1>
                 </div>
-            </div>
-        </div>
+                <div class="cds-9 css-1e8r9bd cds-11 cds-grid-item">
+                    <h2 class="cds-113 cds-Typography-base css-1825nsb cds-115">
+                        "Professional Certificates offer flexible, online training designed to get you job-ready for high-growth fields."
+                    </h2>
+                </div>
+                <div class="cds-9 css-1e8r9bd cds-11 cds-grid-item">
+                    <h3 class="cds-113 cds-Typography-base css-e7lgfl cds-115">
+                        "7-day free trial&emsp;|&emsp;Starting at $49 per month&emsp;|&emsp;Less than 6 months at your own pace"
+                        </h3>
+                </div>
+                <div class="cds-9 css-0 cds-11 cds-grid-item">
+                    <a class="cds-143 cds-button-disableElevation cds-button-primary css-k93wf6" tabindex="0" aria-disabled="false" href="#explore-careers">
+                        <span class="cds-button-label">Explore Careers</span>
+                        </a>
+                </div>
 
-    );
+            </div>
+
+                <Degrees>
+
+                </Degrees>
+            </>
+
+            );
 }
