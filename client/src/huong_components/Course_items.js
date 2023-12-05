6f821da0f9ca098c92a76af99e29858952de23fb
dinhhuong2002 <dinhthihuongdung2002@gmail.com>
2023-12-05 10:19:00 +0700

    create MyCoursePage

--- client/src/huong_components/Course_items.js
+++ client/src/huong_components/Course_items.js
@@ -1,6 +1,6 @@
 export default function Course_items() {
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="course_items">
             <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">
