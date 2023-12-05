bd012c2f09284959eeb60d02aff55eb616126380
TruongHung02 <truongviethung2682002@gmail.com>
2023-12-05 19:27:33 +0700

    update user controller

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -16,6 +16,42 @@ const controller = {
         if (!user_by_id) return res.status(404).json(api_response(true, 'Không tìm thấy người dùng'));
         return res.status(200).json(api_response(false, 'Lấy thông tin người dùng thành công', user_by_id));
     }),
+    // [PATCH] /api/user/:id
+    update_user: async_wrap(async (req, res) => {
+        const user_by_id = await db.User.findByPk(req.params.id);
+        if (!user_by_id) return res.status(404).json(api_response(true, 'Không tìm thấy người dùng'));
+        if (user_by_id.User_ID != req.token.id)
+            return res.status(403).json(api_response(true, 'Bạn không có quyền chỉnh sửa thông tin người dùng này'));
+
+        course.Category_ID = req.body.Category_ID || course.Category_ID;
+        course.Name = req.body.Name || course.Name;
+        course.Description = req.body.Description || course.Description;
+        course.Level = req.body.Level || course.Level;
+        course.Need_Approval = req.body.Need_Approval != undefined ? req.body.Need_Approval : course.Need_Approval;
+        course.Status = req.body.Status || course.Status;
+        if (req.file) {
+            await firebase_service.delete_file(course.Image);
+            course.Image = await firebase_service.upload_image(req.file.path);
+        }
+        await course.save();
+
+        return res.status(200).json(api_response(false, 'Cập nhật thông tin khóa học thành công'));
+    }),
+
+    // [DELETE] /api/course/:id
+    delete_user: async_wrap(async (req, res) => {
+        const course = await db.Course.findByPk(req.params.id);
+        if (!course) return res.status(404).json(api_response(true, 'Không tìm thấy khóa học'));
+        if (course.User_ID != req.token.id)
+            return res.status(403).json(api_response(true, 'Bạn không có quyền chỉnh sửa khóa học này'));
+
+        await firebase_service.delete_file(course.Image);
+        await db.Course.destroy({
+            where: { Course_ID: req.params.id },
+        });
+
+        return res.status(200).json(api_response(false, 'Xóa khóa học thành công'));
+    }),
 
 };
 
