f213fb3fcc44beedf197f7cacada4636124afedd
dinhhuong2002 <dinhthihuongdung2002@gmail.com>
2023-12-06 00:21:44 +0700

    add Career_items for CareerPage

---
+++ client/src/huong_components/Career_items.js
@@ -0,0 +1,20 @@
+export default function Career_items() {
+    return (
+        <div className="course_items">
+            <figure><img src="https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/h…8e1bc20627c12d/cybersecurity-cover.jpg?auto=format%2Ccompress&dpr=1&w=&h= alt="
+                Shoes /></figure>
+            <div className="career-body">
+                <h2 className="title">
+                    Shoes!
+                    <div className="badge badge-secondary">NEW</div>
+                </h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end">
+                    <div className="badge badge-outline">Fashion</div>
+                    <div className="badge badge-outline">Products</div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
