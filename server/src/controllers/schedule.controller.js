00ae4e729ca7f4494e47d6df1a896849a24b57e7
TruongHung02 <truongviethung2682002@gmail.com>
2023-12-07 22:38:33 +0700

    add schedule controller

---
+++ server/src/controllers/schedule.controller.js
@@ -0,0 +1,49 @@
+import db from '../models/index.js';
+import api_response from '../utils/api_response.js';
+import async_wrap from '../utils/async_wrap.js';
+
+const controller = {
+    // [GET] /api/category/
+    get_all_categories: async_wrap(async (req, res) => {
+        const categories = await db.Category.findAll();
+        return res.status(200).json(api_response(false, 'Lấy danh sách danh mục khóa học thành công', categories));
+    }),
+
+    // [GET] /api/category/:id
+    get_category_by_id: async_wrap(async (req, res) => {
+        const category = await db.Category.findByPk(req.params.id);
+        if (!category) return res.status(404).json(api_response(true, 'Không tìm thấy danh mục khóa học'));
+        return res.status(200).json(api_response(false, 'Lấy thông tin danh mục khóa học thành công', category));
+    }),
+
+    // [POST] /api/category/
+    add_category: async_wrap(async (req, res) => {
+        const category = await db.Category.create({
+            Name: req.body.Name,
+        });
+        return res.status(201).json(api_response(false, 'Thêm danh mục khóa học mới thành công', category));
+    }),
+
+    // [PATCH] /api/category/:id
+    update_category: async_wrap(async (req, res) => {
+        const category = await db.Category.findByPk(req.params.id);
+        if (!category) return res.status(404).json(api_response(true, 'Không tìm thấy danh mục khóa học'));
+
+        category.Name = req.body.Name;
+        await category.save();
+
+        return res.status(200).json(api_response(false, 'Cập nhật thông tin danh mục khóa học thành công'));
+    }),
+
+    // [DELETE] /api/category/:id
+    delete_category: async_wrap(async (req, res) => {
+        const result = await db.Category.destroy({
+            where: { Category_ID: req.params.id },
+        });
+
+        if (result === 1) return res.status(200).json(api_response(false, 'Xóa danh mục khóa học thành công'));
+        else return res.status(404).json(api_response(true, 'Không tìm thấy danh mục khóa học'));
+    }),
+};
+
+export default controller;
