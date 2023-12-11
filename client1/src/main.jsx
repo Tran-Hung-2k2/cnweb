a5db33338ad16ba66daa0248c9dca13e6f12a662
Tran Hung <tranviethung912002@gmail.com>
2023-12-11 23:04:59 +0700

    add hassh router

--- client1/src/main.jsx
+++ client1/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
 import store from './redux/store';
