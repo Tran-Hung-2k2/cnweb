ce80b2b34a77447e51a9ce4fcf8ceaee0936ee50
Tran Hung <tranviethung912002@gmail.com>
2023-12-12 00:00:51 +0700

    test deploy

--- client1/vite.config.js
+++ client1/vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite';
 // import mkcert from 'vite-plugin-mkcert'
 import react from '@vitejs/plugin-react';
@@ -5,6 +6,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: './',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+            },
+        },
+    },
 });
 // export default defineConfig({
 //     server: { https: true },
