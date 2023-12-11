34df1986fda536e3d3282cea0bfa79125ead3868
Tran Hung <tranviethung912002@gmail.com>
2023-12-11 18:42:16 +0700

    .

--- client1/vite.config.js
+++ client1/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
-import mkcert from 'vite-plugin-mkcert'
+// import mkcert from 'vite-plugin-mkcert'
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: { https: true },
-    plugins: [react(), mkcert()],
+    plugins: [react()],
 });
+// export default defineConfig({
+//     server: { https: true },
+//     plugins: [react(), mkcert()],
+// });
