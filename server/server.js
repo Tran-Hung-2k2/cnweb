136508d57951bb237f0e6c5c85af2419bf032426
Tran Hung <tranviethung912002@gmail.com>
2023-12-11 20:23:29 +0700

    deploy server

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ['https://localhost:5173', 'http://localhost:5173'],
+        origin: ['https://localhost:5173', 'http://localhost:5173', 'https://coursera-cnweb.onrender.com'],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
         allowedHeaders: [
