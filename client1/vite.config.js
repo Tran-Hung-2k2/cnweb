4972fd4a86b8d88dcff1d065a42d082b524a6b3b
Tran Hung <tranviethung912002@gmail.com>
2023-12-14 05:37:12 +0700

    .

--- client1/vite.config.js
+++ client1/vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: 'https://tranhung912002.id.vn/',
+    // base: 'https://tranhung912002.id.vn/',
     build: {
         outDir: '../server/public/',
         rollupOptions: {
--- server/server.js
+++ server/server.js
@@ -14,6 +14,8 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(
     cors({
         origin: [
