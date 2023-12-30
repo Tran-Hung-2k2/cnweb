f2c6d202f35259cfe2b980899f1cf07cad9126b9
Tran Hung <tranviethung912002@gmail.com>
2023-12-30 23:13:36 +0700

    .

--- client/src/components/RequireUser.jsx
+++ client/src/components/RequireUser.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
-function RequireOrg() {
+function RequireUser() {
     const { user } = useSelector((state) => state.auth);
     const location = useLocation();
 
     return user ? <Outlet /> : <Navigate to="/signin" state={{ from: location }} replace />;
 }
 
-export default RequireOrg;
+export default RequireUser;
